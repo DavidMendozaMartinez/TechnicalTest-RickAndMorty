605342cc12389448114a20d342257d7fe9d3f29a
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2021-10-30 18:58:53 +0200

    Create the screens
    Create the MainActivity class.
    Create the characters, detail and search fragments.

---
+++ app/src/main/java/com/davidmendozamartinez/technicaltest/rickandmorty/ui/screen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.davidmendozamartinez.technicaltest.rickandmorty.ui.screen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.davidmendozamartinez.technicaltest.rickandmorty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/davidmendozamartinez/technicaltest/rickandmorty/ui/screen/characters/CharactersFragment.kt
@@ -0,0 +1,17 @@
+package com.davidmendozamartinez.technicaltest.rickandmorty.ui.screen.characters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.davidmendozamartinez.technicaltest.rickandmorty.R
+
+class CharactersFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_characters, container, false)
+    }
+}
